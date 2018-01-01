f12d3cd4bf77392d0108635df9130e75cb6cf39d
eddie <psyck_o@hotmail.com>
2018-01-01 12:40:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,4 @@
-var meals = {};
-
-var meals = new Object();
-
-var meals = { breakfast: "oatmeal" };
-
-var meals = Object({breakfast: "oatmeal"});
-
-var meals = {
-  breakfast: 'avocado',
-  lunch: 'avocado',
-  dinner: 'avocado'
+var playlist = {
+  maxwell: 'Pretty Wings',
+  sting: 'Red Dress'
 }
