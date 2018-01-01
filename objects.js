982be247ff8692148b5fd10d1c7edc5c5fdb2d12
eddie <psyck_o@hotmail.com>
2018-01-01 13:22:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,7 @@ function updatePlaylist(obj, artistName, songTitle) {
   return Object.assign({}, obj, {[artistName]: songTitle});
 }
 
+function removeFromPlaylist(obj, artistName) {
+ delete obj.artistName;
+ return obj;
+}
