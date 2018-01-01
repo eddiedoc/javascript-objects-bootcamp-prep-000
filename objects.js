790f3d15062ca4f329081852e7ee880c49969289
eddie <psyck_o@hotmail.com>
2018-01-01 12:56:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
 var playlist = {
   maxwell: 'Pretty Wings',
   sting: 'Roxanne',
-  "cindy lauper"
+  "cindy lauper": 'Girls just wanna have fun'
+};
+
+function updatePlaylist(obj, artist, song) {
+  return object.assign({}, obj, {[artist]: song})
 }
+
