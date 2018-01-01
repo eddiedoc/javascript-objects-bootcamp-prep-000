8fe981c6cf91f5a49c6a85d1636e2f541e19286a
eddie <psyck_o@hotmail.com>
2018-01-01 11:58:59 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var meals = {};
+
+var meals = new Object();
+
+var meals = { breakfast: "oatmeal" };
+
+var meals = Object({breakfast: "oatmeal"});
+
+var meals = {
+  breakfast: 'avocado',
+  lunch: 'avocado',
+  dinner: 'avocado'
+}
